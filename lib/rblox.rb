e09e2c56dc110a99b3564d75f8ce5fc1aa2d8737
Yishu See <yishuyishu@gmail.com>
2022-10-01 20:19:21 +0800

    Add rblox bin

--- lib/rblox.rb
+++ lib/rblox.rb
@@ -1,6 +1,21 @@
-require "rblox/version"
-
 module Rblox
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.run_prompt
+    loop do
+      print "> "
+      line = gets
+      break unless line
+      puts line
+    end
+  end
+
+  def self.run_file(path)
+    file = File.open(path)
+    print(file.read)
+  end
+
+  def self.print_usage
+    puts "Usage: rblox [script]"
+  end
 end
